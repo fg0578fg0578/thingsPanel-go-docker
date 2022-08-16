bf9099acf002155aef031af91fcf5f3ac77a83aa
hezhuozhuo <421045249@qq.com>
2022-08-16 10:57:37 +0800

    update

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -6,7 +6,7 @@ set -e
 host="$1"
 password="$2"
 shift
-  
+echo "----------------------------------------------------------------"
 until PGPASSWORD=$password psql -h "$host" -U "postgres" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
